48eaf2da0fc8d22262b0258639fa1956453d2910
Vick <vike11107@gmail.com>
2026-03-14 21:25:39 +0800

    feat: inplement substitution cipher

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
+# Create a namespace for substitution ciphers
 module SubstitutionCipher
+  # Create a module for Caesar cipher
   module Caesar
     # Encrypts document using key
     # Arguments:
@@ -7,6 +11,11 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using caesar cipher
+      json_string = document.to_json
+      encrypted_list = json_string.bytes.map do |byte|
+        (byte + key).chr
+      end
+      encrypted_list.join
     end
 
     # Decrypts String document using integer key
@@ -16,9 +25,14 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using caesar cipher
+      decrypted_list = document.bytes.map do |char|
+        (char - key).chr
+      end
+      decrypted_list.join
     end
   end
 
+  # Create a module for permutation cipher
   module Permutation
     # Encrypts document using key
     # Arguments:
@@ -27,6 +41,13 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+      rng = Random.new(key)
+      table = (0..127).to_a.shuffle(random: rng)
+      json_string = document.to_json
+      encrypted_list = json_string.bytes.map do |byte|
+        table[byte].chr
+      end
+      encrypted_list.join
     end
 
     # Decrypts String document using integer key
@@ -36,6 +57,16 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
+      rng = Random.new(key)
+      table = (0..127).to_a.shuffle(random: rng)
+      inverse_table = Array.new(128)
+      table.each_with_index do |value, index|
+        inverse_table[value] = index
+      end
+      decrypted_list = document.bytes.map do |char|
+        inverse_table[char].chr
+      end
+      decrypted_list.join
     end
   end
 end
